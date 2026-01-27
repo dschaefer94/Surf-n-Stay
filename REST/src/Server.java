c2482f5a3c5589be0928b9b67ad3af88203ccc2b
Daniel Schäfer <daniel.schaefer@edu.bib.de>
2026-01-27 23:25:18 +0100

    REST gerettet

---
+++ REST/src/Server.java
@@ -0,0 +1,16 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+import db.Database;
+import http.UserHandler;
+import http.OfferHandler;
+public class Server {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/user", new UserHandler());
+        server.createContext("/offer", new OfferHandler());
+        Database.initialize();
+        server.start();
+        System.out.println("Server läuft auf http://localhost:8080/offer");
+    }
+}
